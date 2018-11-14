4e4d1642209ecd08203f706f3c303abacd7b570a
Jesper Stemann Andersen <jsa@ihp.dk>
2018-11-14 16:20:00 +0100

    Elaborated on abstract Camera: Opening/closing, getting camera info
    Added CameraInfo and related functions

--- src/Cameras.jl
+++ src/Cameras.jl
@@ -1,14 +1,35 @@
 module Cameras
 
 export Camera,
+    camera_info,
+    isopen,
     isrunning,
+    open!,
+    close!,
     start!,
     stop!
 
+export CameraInfo,
+    vendor_name,
+    model_name,
+    serial_number
+
 abstract type Camera end
+
+isopen(camera::Camera) = error("No implementation for $(typeof(camera))")
+open!(camera::Camera) = error("No implementation for $(typeof(camera))")
+close!(camera::Camera) = error("No implementation for $(typeof(camera))")
+
+camera_info(camera::Camera) = error("No implementation for $(typeof(camera))")
 
 isrunning(camera::Camera) = error("No implementation for $(typeof(camera))")
 start!(camera::Camera) = error("No implementation for $(typeof(camera))")
 stop!(camera::Camera) = error("No implementation for $(typeof(camera))")
+
+abstract type CameraInfo end
+
+vendor_name(info::CameraInfo) = error("No implementation for $(typeof(info))")
+model_name(info::CameraInfo) = error("No implementation for $(typeof(info))")
+serial_number(info::CameraInfo) = error("No implementation for $(typeof(info))")
 
 end # module
